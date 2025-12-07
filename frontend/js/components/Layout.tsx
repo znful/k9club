e8bac69f9177c4a714a5f9e1ba98f3efe2e1b314
David Macario <david.macariopro@gmail.com>
2025-12-07 21:43:59 +0100

    Edit layout

--- frontend/js/components/Layout.tsx
+++ frontend/js/components/Layout.tsx
@@ -1,19 +1,16 @@
 import React from "react";
-import { Link } from "@inertiajs/react";
+import { SidebarProvider, SidebarTrigger, Sidebar } from "@/components/ui/sidebar"
 
-const Layout = ({ children }: React.PropsWithChildren) => (
-	<>
-		<div className="min-h-screen bg-gray-100 p-8 dark:bg-gray-900">
-			<nav className="flex items-start justify-center">
-				<ul className="flex space-x-4">
-					<li>
-						<Link href="/" className="dark: text-white">Home</Link>
-					</li>
-				</ul>
-			</nav>
-			<div className="flex items-center justify-center mt-32">{children}</div>
-		</div>
-	</>
-);
+export function Layout({ children }: { children: React.ReactNode }) {
+	return (
+		<SidebarProvider>
+			<Sidebar />
+			<main>
+				<SidebarTrigger />
+				{children}
+			</main>
+		</SidebarProvider>
+	)
+}
 
 export default (page) => <Layout>{page}</Layout>;
