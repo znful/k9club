2562eff4acbe3df2b5f2c3f9129d477f1022e547
David Macario <david.macariopro@gmail.com>
2025-12-09 23:03:42 +0100

    update clubs#index view

--- frontend/js/pages/Clubs/Index.tsx
+++ frontend/js/pages/Clubs/Index.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import Layout from '@/layouts/app-layout';
 import type { Club } from '@/types';
 
-export default function Index({ clubs }: { clubs: Club[] }) {
+interface Props {
+  clubs?: Partial<Club>[]
+}
+
+export default function Index({ clubs }: Props) {
   return (
     <Layout>
       <div className="">
         <h2 className="text-3xl font-bold underline">Clubs</h2>
-        <p className="text-lg font-mono">
-          {clubs.map((club) => (
-            <h1 key={club.id}>{club.name}</h1>
-          ))}
-        </p>
+        {clubs != undefined && clubs.map((club) => (
+          <h1 key={club.id}>{club.name}</h1>
+        ))}
       </div>
     </Layout>
   );
--- k9club/apps/core/views.py
+++ k9club/apps/core/views.py
@@ -1,7 +1,13 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
 from inertia import render
 from inertia.http import InertiaRequest
 
+from k9club.apps.core.models import Club
 
-# Create your views here.
+
+@login_required
 def club_index(request: InertiaRequest):
-    return render(request=request, component="Clubs/Index", props={})
+    user = request.user
+    clubs = Club.objects.filter(members=user).order_by("-created_at")
+    return render(request=request, component="Clubs/Index", props={"clubs": clubs})
